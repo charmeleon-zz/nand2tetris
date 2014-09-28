f943646a7ac738dd3d35c4a0081944b0a268ec1a
Erick Rivas <chemical.rivas@gmail.com>
2014-09-28 14:01:48 -0700

    Ensuring that two passes are made when translating .asm files to .hack

--- 06/Assembler.py
+++ 06/Assembler.py
@@ -17,28 +17,29 @@ class Assembler:
         return len(self.files) > 0
 
     def dummyPass(self):
-        """Do a soft pass to determine the location for lables (Xxx)"""
+        """Do a soft pass to determine the location for labels (Xxx)"""
         filename = self.files[0]
         p = Parser(filename)
         current_address = 0
         while p.hasMoreCommands():
             p.advance()
-            if p.commandType() is A_COMMAND or p.commandType() is C_COMMAND:
+            cmd_type = p.commandType()
+            if cmd_type is A_COMMAND or cmd_type is C_COMMAND:
                 current_address += 1
-            else:
+            elif cmd_type is L_COMMAND:
                 self.symtable.addEntry(p.symbol(), current_address)
 
     def processFile(self):
         """Translates the next file in the queue"""
         filename = self.files.pop(0)
         p = Parser(filename)
-        self.current_address = 16
         if filename.endswith('.asm'):
             fileout = filename.replace('.asm', '.hack')
         else:
             fileout = filename + '.hack'
         f = open(fileout, 'w')
         print("Translating %s" % (filename))
+        self.current_address = 16
         while p.hasMoreCommands():
             p.advance()
             if p.commandType() is A_COMMAND:
@@ -68,5 +69,6 @@ if __name__ == '__main__':
         sys.exit(1) 
     a = Assembler(sys.argv[1:])
     while a.hasFiles():
+        a.dummyPass()
         a.processFile()
 
